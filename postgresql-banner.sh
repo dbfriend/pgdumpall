31df30dea201bbe9687221edf1f76bd3e18a6f02
Matthias <118447736+dbfriend@users.noreply.github.com>
2024-02-15 08:56:11 +0100

    Update postgresql-banner.sh

--- postgresql-banner.sh
+++ postgresql-banner.sh
@@ -1,6 +1,6 @@
-!/bin/bash
+#!/bin/bash
 # Script name: postgresql-banner.sh
-# Description: This small script shows some basic information about the PostgreSQL database
+# Description: This small script shows some basic information about the PostgreSQL database. Should be placed at location /etc/profile.d/ with 644 permissions
 # Version: 1.0
 
 PGDATALOC=$(systemctl cat postgresql.service | grep "Environment=PGDATA=" | awk -F'=' '{print $3}')
