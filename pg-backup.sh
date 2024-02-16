56ebdc77d0c8a77f93d54c89404ee22951e95abc
Matthias <118447736+dbfriend@users.noreply.github.com>
2024-02-16 14:24:18 +0100

    Update pg-backup.sh

--- pg-backup.sh
+++ pg-backup.sh
@@ -6,7 +6,7 @@ USER=backup
 POSTFIX=${HOSTNAME}_$(date +"%Y%m%d%H%M")
 PGDUMPALL=/usr/bin/pg_dumpall
 CLEANLOG=${BACKUPLOC}/pgdumpall_${POSTFIX}.clog
-export PGPASSFILE=/var/lib/pgsql/.pgpass
+export PGPASSFILE=/home/postgres/.pgpass
 
 ### Do the actual work
 ${PGDUMPALL} -v -c -U ${USER} -f ${BACKUPLOC}/pgdumpall_${POSTFIX}.sql 2> ${BACKUPLOC}/pgdumpall_${POSTFIX}.log
